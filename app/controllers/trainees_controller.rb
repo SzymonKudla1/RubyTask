c280833ff19ff6994fc6d0d0e3b6dad80aa97617
SzymonKudla1 <kudla.szymon5@gmail.com>
2021-10-22 22:11:00 +0200

    Final version

--- app/controllers/trainees_controller.rb
+++ app/controllers/trainees_controller.rb
@@ -5,32 +5,41 @@ class TraineesController < ApplicationController
     @q = Trainee.ransack(params[:q])
     @trainees = @q.result(distinct: true)
   end  
+
   def new
     @trainee = Trainee.new
   end
+
   def create
     @trainee = Trainee.new(trainee_params)
+    
     if @trainee.save
       redirect_to trainees_path
     else
       render :new
     end      
   end
+
   def edit
     @trainee = Trainee.find(params[:id])
   end
+
   def update
     @trainee = Trainee.find(params[:id])
     @trainee.assign_attributes(trainee_params)
+
     if @trainee.save
       redirect_to trainees_path
     end
   end  
+
   def show
     @trainee = Trainee.find(params[:id])
   end  
+
   private
+
   def trainee_params
-    params.require(:trainee).permit(:first_name, :last_name, :email, :birthdate, :active)
+    params.require(:trainee).permit(:first_name, :last_name, :email, :birthdate, :active, course_ids: [])
   end
 end
